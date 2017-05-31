10c4be86df0d6c6a5d9e5ea22d41afe0cea642f2
ADOBENET\ramasamy <ramasamy@adobe.com>
2017-05-31 19:01:28 +0530

    Test commit 4

--- MultiThreadingDemo/src/redeem-gift-card-controller.js
+++ MultiThreadingDemo/src/redeem-gift-card-controller.js
@@ -1,38 +1,38 @@
 /**
  * @file Redeem Gift Card Controller 
  */
-(function () {
+(function() {
     'use strict';
 
     function redeemGiftCardController($scope, $rootScope, $location, $window, applicationState, userProfileService, rewardsService, linkService, accountSummaryService) {
         /* jshint validthis: true */
         var vm = this;
 
-        $scope.$watch(function () {
+        $scope.$watch(function() {
             return applicationState.data.loggedIn;
         }, function watchCallback(newValue, oldValue) {
             if (!newValue) {
                 vm.redirectBackToParentPage();
-            } 			
-        }, true);           
+            }
+        }, true);
 
-        function initUserInfo(isCacheClear, redirectUrl) {	
+        function initUserInfo(isCacheClear, redirectUrl) {
             vm.redirectPageUrl = redirectUrl;
-            if (rewardsService.selectedReward.rewardId) {	                                  
+            if (rewardsService.selectedReward.rewardId) {
                 vm.quantity = rewardsService.selectedReward.quantity;
                 vm.rewardDetails = rewardsService.selectedReward;
                 if (applicationState.data.loggedIn) {
                     var securityTokenPromise = applicationState.getSecurityToken();
-                    securityTokenPromise.then(function (securityToken) {
-                        userProfileService.getBasicUserInfo(securityToken, isCacheClear).then(function (userInfo) {
+                    securityTokenPromise.then(function(securityToken) {
+                        userProfileService.getBasicUserInfo(securityToken, isCacheClear).then(function(userInfo) {
                             vm.basicUserInfo = userInfo;
                             vm.address = userInfo.address;
                         });
                     });
-                    accountSummaryService.getPointsSummary().then(function (pointSummary) {
+                    accountSummaryService.getPointsSummary().then(function(pointSummary) {
                         vm.currentUserPointsBalance = pointSummary.currentPointsBalance;
                         calculateTotalPoints();
-                    }); 
+                    });
 
                 }
             } else {
@@ -51,10 +51,10 @@
             }
         }
 
-        function redirectToEditProfile(url) {            
+        function redirectToEditProfile(url) {
             if (url.trim().length > 0) {
-                userProfileService.editFlag = true;                
-                $location.path(url);       
+                userProfileService.editFlag = true;
+                $location.path(url);
             }
         }
 
@@ -64,13 +64,13 @@
             }
         }
 
-        $scope.increase = function (quantity) {
+        $scope.increase = function(quantity) {
             if (quantity < 9) {
                 vm.quantity++;
                 calculateTotalPoints();
             }
         };
-        $scope.decrease = function (quantity) {
+        $scope.decrease = function(quantity) {
             if (quantity > 1) {
                 vm.quantity--;
                 calculateTotalPoints();
@@ -79,66 +79,67 @@
 
         function calculateTotalPoints() {
             if (vm.quantity && vm.rewardDetails.points) {
-                vm.totalPoints = vm.quantity * vm.rewardDetails.points;                
+                vm.totalPoints = vm.quantity * vm.rewardDetails.points;
             }
             validateUserPointsForRedeem();
-        }        
+        }
 
         function validateUserPointsForRedeem() {
-            if(vm.totalPoints > vm.currentUserPointsBalance) {
+            if (vm.totalPoints > vm.currentUserPointsBalance) {
                 vm.insufficientPointsFlag = true;
             } else {
                 vm.insufficientPointsFlag = false;
-            }           
+            }
         }
 
         function redeemOrder(redirectUrl) {
-            rewardsService.redeemOrderReward().then(                   
+            rewardsService.redeemOrderReward().then(
                 function successCallback(response) {
                     if (response.status === 200 && redirectUrl.trim().length > 0) {
-                        $rootScope.$broadcast('redeem-points',vm.currentUserPointsBalance);
-                        window.location.href = linkService.initLink(redirectUrl);		   
-                    } 
-                }, function errorCallback(response) {
-                    response.then(function(response){                      
+                        $rootScope.$broadcast('redeem-points', vm.currentUserPointsBalance);
+                        window.location.href = linkService.initLink(redirectUrl);
+                    }
+                },
+                function errorCallback(response) {
+                    response.then(function(response) {
                         if (getErrorCodeFromResponse(response) === 422) {
-                            vm.insufficientPointsFlag = true; 
-                        } else {                         
+                            vm.insufficientPointsFlag = true;
+                        } else {
                             vm.redeemFailureFlag = true;
                             vm.errorMessage = response;
                         }
-                    });              
-                });        
-        } 
+                    });
+                });
+        }
 
         function getErrorCodeFromResponse(response) {
             var errorCode = "";
-            if(response.indexOf(";") > 0 && response.indexOf("-") > 0) {
+            if (response.indexOf(";") > 0 && response.indexOf("-") > 0) {
                 var errorString = response.split(";")[1].split("-")[0].trim();
-                if(errorString && errorString.length > 0) {
+                if (errorString && errorString.length > 0) {
                     errorCode = parseInt(errorString);
                 }
-            } 
+            }
             return errorCode;
         }
 
         //--------------------- < View model exposed >
 
         vm.basicUserInfo = {};
-        vm.address = {};     
+        vm.address = {};
         vm.rewardDetails = {};
-        vm.quantity = {}; 
+        vm.quantity = {};
         vm.redirectPageUrl = {};
-        vm.totalPoints = 0;	
+        vm.totalPoints = 0;
         vm.currentUserPointsBalance = {};
         vm.errorMessage = {};
         vm.redeemFailureFlag = false;
-        vm.insufficientPointsFlag = false;        
+        vm.insufficientPointsFlag = false;
         vm.initUserInfo = initUserInfo;
         vm.redeemOrder = redeemOrder;
         vm.navigate = navigate;
         vm.redirectToEditProfile = redirectToEditProfile;
-        vm.redirectBackToParentPage = redirectBackToParentPage;        
+        vm.redirectBackToParentPage = redirectBackToParentPage;
     }
     lqModule.controller('redeemGiftCardController', redeemGiftCardController);
     redeemGiftCardController.$inject = ['$scope', '$rootScope', '$location', '$window', 'applicationState', 'userProfileService', 'rewardsService', 'linkService', 'accountSummaryService'];
